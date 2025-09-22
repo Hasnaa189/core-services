428de2c7e36e1ea99416e72a82ed74213cb0afea
Hasnaa189 <152676763+Hasnaa189@users.noreply.github.com>
2025-09-22 19:01:13 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// عشان نفتح ونقفل المينيو في الموبايل
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
+
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
+
+
+
+// لما يحصل scroll ضيف/شيل الكلاس
+window.addEventListener("scroll", function () {
+  const navbar = document.querySelector(".navbar");
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
